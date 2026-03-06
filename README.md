5f0dda9fc3008361b9ae95ac73e41e0f2a1a2b01
Gur-Sych <149085903+GurSych@users.noreply.github.com>
2026-03-06 18:00:31 +0300

    Library 2.0.0 version
    - Add variadic options to tcl::colorize
    - Strongly type OptionList as enum class
    - Update CMakeLists.txt to requare C++20
    - Update README.md with docs

--- README.md
+++ README.md
@@ -1,2 +1,154 @@
 # TinyColor
 My tiny header-only library for terminal coloring
+
+# Using Library
+
+## Library connection
+
+```cpp
+#include "TinyColor/TinyColor.hpp"
+
+namespace tcl = gst::tcl; // for convenience
+```
+
+> [!NOTE]
+> All library functions are in `gst::tcl::` namespace, so that's convenient to use namespace aliases
+
+> [!IMPORTANT]
+> This library requires C++20 standard
+
+### CMake fetching
+
+Library is ready to be fetched using CMake's `fetch_content`
+
+```cmake
+include(FetchContent)
+
+fetch_content(TinyColor
+    GIT_REPOSITORY https://github.com/GurSych/TinyColor
+    GIT_TAG        v2.0.0
+)
+FetchContent_MakeAvailable(TinyColor)
+
+target_link_libraries(foo PRIVATE TinyColor::TinyColor)
+```
+
+## Usage documentation
+
+> Library is in development so this section is updated together with library itself
+
+### Terminal coloring & styling
+
+TinyColor provides a simple way to color and style terminal output using functions:<br>
+
+`void set(tcl::OptionList option, std::ostream& stream = std::cout)`<br>
+`void set(std::initializer_list<tcl::OptionList> options, std::ostream& stream = std::cout)`<br>
+`void reset(std::ostream& stream = std::cout)`
+
+```cpp
+tcl::set(tcl::RED);                 // tcl::set(tcl::RED, std::cout);
+std::cout << "This text is red" << std::endl;
+
+tcl::set({tcl::RED, tcl::BOLD});    // tcl::set({tcl::RED, tcl::BOLD}, std::cout);
+std::cout << "This text is red and bold" << std::endl;
+
+tcl::reset();                       // tcl::reset(std::cout);
+std::cout << "This text is back to normal" << std::endl;
+```
+
+These functions allow you to set terminal color and style options for any `std::ostream` (`std::cout` by default).<br>
+
+### String colorization
+
+`std::string colorize(const std::string& text, tcl::OptionList... option)`<br>
+`std::string colorize(const std::string& text, std::initializer_list<tcl::OptionList> options)`
+
+```cpp
+std::cout << tcl::colorize("This text is red", tcl::RED) << std::endl;
+
+std::cout << tcl::colorize("This text is red and bold", tcl::RED, tcl::BOLD) << std::endl;
+
+std::cout << tcl::colorize("This text is red and bold", {tcl::RED, tcl::BOLD}) << std::endl;
+```
+
+These functions allow you to colorize a string with terminal color and style options.
+
+### Color codes enumeration
+
+All functions considered above use color codes from `tcl::OptionList` enumeration
+
+    RESET               (0)   Reset all attributes
+    BOLD                (1)   Make text bold
+    DIM                 (2)   Make text dim
+    ITALIC              (3)   Make text italic
+    UNDERLINE           (4)   Make text underline
+    BLINK               (5)   Make text blink
+    REVERSE             (7)   Reverse text and background colors
+    CONCEAL             (8)   Conceal text
+    STRIKETHROUGH       (9)   Make text strikethrough
+      
+    DBL_UNDERLINE       (21)  Make text double underline
+    NRM_INTENSITY       (22)  Revert to normal intensity
+    NO_ITALIC           (23)  Revert italic
+    NO_UNDERLINE        (24)  Revert underline
+    NO_BLINK            (25)  Revert blink
+    NO_REVERSE          (27)  Revert reverse
+    NO_CONCEAL          (28)  Revert conceal
+    NO_STRIKETHROUGH    (29)  Revert strikethrough
+       
+    BLACK               (30)  Color text black
+    RED                 (31)  Color text red
+    GREEN               (32)  Color text green
+    YELLOW              (33)  Color text yellow
+    BLUE                (34)  Color text blue
+    MAGENTA             (35)  Color text magenta
+    CYAN                (36)  Color text cyan
+    WHITE               (37)  Color text white
+    DEFAULT             (39)  Color text default
+       
+    BKG_BLACK           (40)  Color background black
+    BKG_RED             (41)  Color background red
+    BKG_GREEN           (42)  Color background green
+    BKG_YELLOW          (43)  Color background yellow
+    BKG_BLUE            (44)  Color background blue
+    BKG_MAGENTA         (45)  Color background magenta
+    BKG_CYAN            (46)  Color background cyan
+    BKG_WHITE           (47)  Color background white
+       
+    BRT_BLACK           (90)  Color text bright black
+    BRT_RED             (91)  Color text bright red
+    BRT_GREEN           (92)  Color text bright green
+    BRT_YELLOW          (93)  Color text bright yellow
+    BRT_BLUE            (94)  Color text bright blue
+    BRT_MAGENTA         (95)  Color text bright magenta
+    BRT_CYAN            (96)  Color text bright cyan
+    BRT_WHITE           (97)  Color text bright white
+      
+    BRT_BKG_BLACK       (100) Color background bright black
+    BRT_BKG_RED         (101) Color background bright red
+    BRT_BKG_GREEN       (102) Color background bright green
+    BRT_BKG_YELLOW      (103) Color background bright yellow
+    BRT_BKG_BLUE        (104) Color background bright blue
+    BRT_BKG_MAGENTA     (105) Color background bright magenta
+    BRT_BKG_CYAN        (106) Color background bright cyan
+    BRT_BKG_WHITE       (107) Color background bright white
+
+### Moving cursor
+
+`std::string up(int8_t lines = 1)`<br>
+`std::string down(int8_t lines = 1)`<br>
+`std::string left(int8_t columns = 1)`<br>
+`std::string right(int8_t columns = 1)`
+
+```cpp
+std::cout << tcl::up();         // moves up 1 line
+std::cout << tcl::down(1);      // moves down 1 line
+std::cout << tcl::left(5);      // moves left 5 columns
+std::cout << tcl::right(-4);    // moves left 4 columns
+```
+
+These functions return a string that moves the cursor up, down, left, or right by the specified number of lines or columns (default is 1).
+
+`std::string clear_line()`<br>
+
+This function returns a string that clears the current line.
--- include/TinyColor/TinyColor.hpp
+++ include/TinyColor/TinyColor.hpp
@@ -8,156 +8,232 @@
 
 #include <initializer_list>
 #include <iostream>
+#include <optional>
 #include <cstdint>
 #include <string>
 
-namespace tcl {
+namespace gst {
 
-    enum OptionList {
+    namespace tcl {
 
-        RESET         = 0,
-        BOLD          = 1,
-        DIM           = 2,
-        ITALIC        = 3,
-        UNDERLINE     = 4,
-        BLINK         = 5,
-        REVERSE       = 7,
-        CONCEAL       = 8,
-        STRIKETHROUGH = 9,
+        enum class OptionList: int8_t {
 
-        DBL_UNDERLINE    = 21,
-        NRM_INTENSITY    = 22,
-        NO_ITALIC        = 23,
-        NO_UNDERLINE     = 24,
-        NO_BLINK         = 25,
-        NO_REVERSE       = 27,
-        NO_CONCEAL       = 28,
-        NO_STRIKETHROUGH = 29,
+            RESET         = 0,
+            BOLD          = 1,
+            DIM           = 2,
+            ITALIC        = 3,
+            UNDERLINE     = 4,
+            BLINK         = 5,
+            REVERSE       = 7,
+            CONCEAL       = 8,
+            STRIKETHROUGH = 9,
 
-        BLACK       = 30,
-        RED         = 31,
-        GREEN       = 32,
-        YELLOW      = 33,
-        BLUE        = 34,
-        MAGENTA     = 35,
-        CYAN        = 36,
-        WHITE       = 37,
-        DEFAULT     = 39,
+            DBL_UNDERLINE    = 21,
+            NRM_INTENSITY    = 22,
+            NO_ITALIC        = 23,
+            NO_UNDERLINE     = 24,
+            NO_BLINK         = 25,
+            NO_REVERSE       = 27,
+            NO_CONCEAL       = 28,
+            NO_STRIKETHROUGH = 29,
 
-        BKG_BLACK   = 40,
-        BKG_RED     = 41,
-        BKG_GREEN   = 42,
-        BKG_YELLOW  = 43,
-        BKG_BLUE    = 44,
-        BKG_MAGENTA = 45,
-        BKG_CYAN    = 46,
-        BKG_WHITE   = 47,
+            BLACK       = 30,
+            RED         = 31,
+            GREEN       = 32,
+            YELLOW      = 33,
+            BLUE        = 34,
+            MAGENTA     = 35,
+            CYAN        = 36,
+            WHITE       = 37,
+            DEFAULT     = 39,
 
-        BRT_BLACK   = 90,
-        BRT_RED     = 91,
-        BRT_GREEN   = 92,
-        BRT_YELLOW  = 93,
-        BRT_BLUE    = 94,
-        BRT_MAGENTA = 95,
-        BRT_CYAN    = 96,
-        BRT_WHITE   = 97,
+            BKG_BLACK   = 40,
+            BKG_RED     = 41,
+            BKG_GREEN   = 42,
+            BKG_YELLOW  = 43,
+            BKG_BLUE    = 44,
+            BKG_MAGENTA = 45,
+            BKG_CYAN    = 46,
+            BKG_WHITE   = 47,
 
-        BRT_BKG_BLACK   = 100,
-        BRT_BKG_RED     = 101,
-        BRT_BKG_GREEN   = 102,
-        BRT_BKG_YELLOW  = 103,
-        BRT_BKG_BLUE    = 104,
-        BRT_BKG_MAGENTA = 105,
-        BRT_BKG_CYAN    = 106,
-        BRT_BKG_WHITE   = 107,
+            BRT_BLACK   = 90,
+            BRT_RED     = 91,
+            BRT_GREEN   = 92,
+            BRT_YELLOW  = 93,
+            BRT_BLUE    = 94,
+            BRT_MAGENTA = 95,
+            BRT_CYAN    = 96,
+            BRT_WHITE   = 97,
 
-    };
+            BRT_BKG_BLACK   = 100,
+            BRT_BKG_RED     = 101,
+            BRT_BKG_GREEN   = 102,
+            BRT_BKG_YELLOW  = 103,
+            BRT_BKG_BLUE    = 104,
+            BRT_BKG_MAGENTA = 105,
+            BRT_BKG_CYAN    = 106,
+            BRT_BKG_WHITE   = 107,
 
-    inline void set(OptionList option, std::ostream& stream = std::cout);
-    inline void set(std::initializer_list<OptionList> options, std::ostream& stream = std::cout);
-    inline void reset(std::ostream& stream = std::cout);
+        };
 
-    inline std::string colorize(const std::string& text, OptionList option);
-    inline std::string colorize(const std::string& text, std::initializer_list<OptionList> options);
+        constexpr auto RESET            = OptionList::RESET;
+        constexpr auto BOLD             = OptionList::BOLD;
+        constexpr auto DIM              = OptionList::DIM;
+        constexpr auto ITALIC           = OptionList::ITALIC;
+        constexpr auto UNDERLINE        = OptionList::UNDERLINE;
+        constexpr auto BLINK            = OptionList::BLINK;
+        constexpr auto REVERSE          = OptionList::REVERSE;
+        constexpr auto CONCEAL          = OptionList::CONCEAL;
+        constexpr auto STRIKETHROUGH    = OptionList::STRIKETHROUGH;
+        constexpr auto DBL_UNDERLINE    = OptionList::DBL_UNDERLINE;
+        constexpr auto NRM_INTENSITY    = OptionList::NRM_INTENSITY;
+        constexpr auto NO_ITALIC        = OptionList::NO_ITALIC;
+        constexpr auto NO_UNDERLINE     = OptionList::NO_UNDERLINE;
+        constexpr auto NO_BLINK         = OptionList::NO_BLINK;
+        constexpr auto NO_REVERSE       = OptionList::NO_REVERSE;
+        constexpr auto NO_CONCEAL       = OptionList::NO_CONCEAL;
+        constexpr auto NO_STRIKETHROUGH = OptionList::NO_STRIKETHROUGH;
+        constexpr auto BLACK            = OptionList::BLACK;
+        constexpr auto RED              = OptionList::RED;
+        constexpr auto GREEN            = OptionList::GREEN;
+        constexpr auto YELLOW           = OptionList::YELLOW;
+        constexpr auto BLUE             = OptionList::BLUE;
+        constexpr auto MAGENTA          = OptionList::MAGENTA;
+        constexpr auto CYAN             = OptionList::CYAN;
+        constexpr auto WHITE            = OptionList::WHITE;
+        constexpr auto DEFAULT          = OptionList::DEFAULT;
+        constexpr auto BKG_BLACK        = OptionList::BKG_BLACK;
+        constexpr auto BKG_RED          = OptionList::BKG_RED;
+        constexpr auto BKG_GREEN        = OptionList::BKG_GREEN;
+        constexpr auto BKG_YELLOW       = OptionList::BKG_YELLOW;
+        constexpr auto BKG_BLUE         = OptionList::BKG_BLUE;
+        constexpr auto BKG_MAGENTA      = OptionList::BKG_MAGENTA;
+        constexpr auto BKG_CYAN         = OptionList::BKG_CYAN;
+        constexpr auto BKG_WHITE        = OptionList::BKG_WHITE;
+        constexpr auto BRT_BLACK        = OptionList::BRT_BLACK;
+        constexpr auto BRT_RED          = OptionList::BRT_RED;
+        constexpr auto BRT_GREEN        = OptionList::BRT_GREEN;
+        constexpr auto BRT_YELLOW       = OptionList::BRT_YELLOW;
+        constexpr auto BRT_BLUE         = OptionList::BRT_BLUE;
+        constexpr auto BRT_MAGENTA      = OptionList::BRT_MAGENTA;
+        constexpr auto BRT_CYAN         = OptionList::BRT_CYAN;
+        constexpr auto BRT_WHITE        = OptionList::BRT_WHITE;
+        constexpr auto BRT_BKG_BLACK    = OptionList::BRT_BKG_BLACK;
+        constexpr auto BRT_BKG_RED      = OptionList::BRT_BKG_RED;
+        constexpr auto BRT_BKG_GREEN    = OptionList::BRT_BKG_GREEN;
+        constexpr auto BRT_BKG_YELLOW   = OptionList::BRT_BKG_YELLOW;
+        constexpr auto BRT_BKG_BLUE     = OptionList::BRT_BKG_BLUE;
+        constexpr auto BRT_BKG_MAGENTA  = OptionList::BRT_BKG_MAGENTA;
+        constexpr auto BRT_BKG_CYAN     = OptionList::BRT_BKG_CYAN;
+        constexpr auto BRT_BKG_WHITE    = OptionList::BRT_BKG_WHITE;
 
-    inline std::string clear_line();
-    inline std::string up(int16_t lines);
-    inline std::string down(int16_t lines);
-    inline std::string left(int16_t columns);
-    inline std::string right(int16_t columns);
+        inline int8_t option_to_ANSI(OptionList option) {
+            return static_cast<int8_t>(option);
+        }
+        inline std::string option_to_ANSI_str(OptionList option) {
+            return std::to_string(option_to_ANSI(option));
+        }
+
+        template<typename... Ts>
+        concept AllOptionLists = (std::same_as<Ts, OptionList> && ...);
+
+        inline void set(OptionList option, std::ostream& stream = std::cout);
+        inline void set(std::initializer_list<OptionList> options, std::ostream& stream = std::cout);
+        inline void reset(std::ostream& stream = std::cout);
+
+        template<AllOptionLists... OTs>
+        inline std::string colorize(const std::string& text, OTs... options);
+        inline std::string colorize(const std::string& text, std::initializer_list<OptionList> options);
+
+        inline std::string clear_line();
+        inline std::string up(int8_t lines = 1);
+        inline std::string down(int8_t lines = 1);
+        inline std::string left(int8_t columns = 1);
+        inline std::string right(int8_t columns = 1);
+
+    }
 
 }
 
-inline void tcl::set(OptionList option, std::ostream& stream) {
-    stream << "\033[" << option << "m";
+inline void gst::tcl::set(gst::tcl::OptionList option, std::ostream& stream) {
+    stream << "\033[" << option_to_ANSI_str(option) << "m";
 }
 
-inline void tcl::set(std::initializer_list<OptionList> options, std::ostream& stream) {
-    std::string out = "\033[";
+inline void gst::tcl::set(std::initializer_list<gst::tcl::OptionList> options, std::ostream& stream) {
+    if (options.size() == 0) {
+        return;
+    }
+    std::string out{"\033["};
     for (auto option : options) {
-        out += std::to_string(option) + ";";
+        out += option_to_ANSI_str(option) + ';';
     }
     out.pop_back();
     out += "m";
     stream << out;
 }
 
-inline void tcl::reset(std::ostream& stream) {
-    stream << "\033[" << RESET << "m";
+inline void gst::tcl::reset(std::ostream& stream) {
+    stream << "\033[0m";
 }
 
-inline std::string tcl::colorize(const std::string& text, OptionList option) {
-    std::string out = "\033[";
-    out += std::to_string(option) + "m" + text + "\033[" + std::to_string(RESET) + "m";
+template<gst::tcl::AllOptionLists... OTs>
+inline std::string gst::tcl::colorize(const std::string& text, OTs... options) {
+    if constexpr (sizeof...(options) == 0) {
+        return text;
+    }
+    std::string out{"\033["};
+    ((out += option_to_ANSI_str(options) + ';'), ...);
+    out.pop_back();
+    out += "m" + text + "\033[0m";
     return out;
 }
 
-inline std::string tcl::colorize(const std::string& text, std::initializer_list<OptionList> options) {
+inline std::string gst::tcl::colorize(const std::string& text, std::initializer_list<gst::tcl::OptionList> options) {
     std::string out = "\033[";
     for (auto option : options) {
-        out += std::to_string(option) + ";";
+        out += option_to_ANSI_str(option) + ';';
     }
     out.pop_back();
-    out += "m" + text + "\033[" + std::to_string(RESET) + "m";
+    out += "m" + text + "\033[0m";
     return out;
 }
 
-inline std::string tcl::clear_line() {
+inline std::string gst::tcl::clear_line() {
     return "\033[2K";
 }
 
-inline std::string tcl::up(int16_t lines) {
+inline std::string gst::tcl::up(int8_t lines) {
     if (lines < 0) {
-        return tcl::down(-lines);
+        return gst::tcl::down(-lines);
     }
-    std::string out = "\033[";
+    std::string out{"\033["};
     out += std::to_string(lines) + "A";
     return out;
 }
 
-inline std::string tcl::down(int16_t lines) {
+inline std::string gst::tcl::down(int8_t lines) {
     if (lines < 0) {
-        return tcl::up(-lines);
+        return gst::tcl::up(-lines);
     }
-    std::string out = "\033[";
+    std::string out{"\033["};
     out += std::to_string(lines) + "B";
     return out;
 }
 
-inline std::string tcl::left(int16_t columns) {
+inline std::string gst::tcl::left(int8_t columns) {
     if (columns < 0) {
-        return tcl::right(-columns);
+        return gst::tcl::right(-columns);
     }
-    std::string out = "\033[";
+    std::string out{"\033["};
     out += std::to_string(columns) + "D";
     return out;
 }
 
-inline std::string tcl::right(int16_t columns) {
+inline std::string gst::tcl::right(int8_t columns) {
     if (columns < 0) {
-        return tcl::left(-columns);
+        return gst::tcl::left(-columns);
     }
-    std::string out = "\033[";
+    std::string out{"\033["};
     out += std::to_string(columns) + "C";
     return out;
 }
